f3424d63fbf3c8210ae142ef310cc51f4f55d128
Javier Castro <javiercastro.ik@gmail.com>
2026-08-20 18:53:20 -0500

    update, remove tailwind, remove variable countdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   compressHTML: false,
   build: {
     assets: '_assets',
     assetsPrefix: 'https://www.claro.com.pe/assets/havas/banner_cintillo'
-  },
-  vite: {
-    plugins: [tailwindcss()]
   }
 });
